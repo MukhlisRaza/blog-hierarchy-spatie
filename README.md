5736b8aa5844f280d0c66d62c1e7bdf62d411ece
Mukhlis Raza <mukhlis.raza1998@gmail.com>
2021-06-29 09:35:37 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# blog-hierarchy-spatie
+ It is a simple blogging application where one is the super-admin how can register blogger and can view blogger details their posts and then blogger have their dashbaord where bloger can register different moderator for their blogs to maintain.
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Publish Posts</div>
+
+                <div class="card-body">
+
+                </div>
+
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">Other Post</div>
+
+                <div class="card-body">
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
